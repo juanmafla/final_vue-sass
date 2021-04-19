199984073fba085aaf5e32741819adfbc719273f
Juan Mafla <juanmafla@gmail.com>
2021-04-19 15:49:56 -0500

    textarea

--- src/App.vue
+++ src/App.vue
@@ -98,15 +98,10 @@
           </div>
         </div>
 
-        <div class="row2">
+        <div class="row2  input-wrapper">
           <h4>Describe Your Delivery Details</h4>
-          <p class="para">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-          </p>
+          <textarea class="para" maxlength="200">
+          </textarea>
           <p class="limit">200 words max</p>
         </div>
 
